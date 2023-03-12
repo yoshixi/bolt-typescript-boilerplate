e138cd86e6e8bf5bb71eab06a11f6147b477209b
yoshiki masubuchi <yoshixi.dev@gmail.com>
2023-03-12 15:44:28 +0900

    Update script

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,31 @@
 /* eslint-disable no-console */
 /* eslint-disable import/no-internal-modules */
 import './utils/env';
-import { App, LogLevel } from '@slack/bolt';
+import { App, LogLevel, ExpressReceiver } from '@slack/bolt';
 import { isGenericMessageEvent } from './utils/helper';
+
+const receiver = new ExpressReceiver({ signingSecret: process.env['SLACK_SIGNING_SECRET'] as string });
 
 // @ts-ignore
 const app = new App({
   token: process.env['SLACK_BOT_TOKEN'],
   signingSecret: process.env['SLACK_SIGNING_SECRET'],
+  receiver,
   logLevel: LogLevel.DEBUG,
 });
 
 app.use(async ({ next }) => {
   await next!();
+});
+
+receiver.router.get('/ping', (_req, res) => {
+  // You're working with an express req and res now.
+  res.send('yay!');
+});
+
+receiver.router.get('/', (_req, res) => {
+  // You're working with an express req and res now.
+  res.send('yay!');
 });
 
 // Listens to incoming messages that contain "hello"
@@ -50,7 +63,8 @@ app.action('button_click', async ({ body, ack, say }) => {
 
 (async () => {
   // Start your app
-  await app.start(Number(process.env['PORT']) || 3000);
+  const port = Number(process.env['PORT']) || 8080
+  await app.start(port);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log(`⚡️ Bolt app is running on port ${port}!`);
 })();
